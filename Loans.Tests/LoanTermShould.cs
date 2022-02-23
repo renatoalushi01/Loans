f85e20d9e3b8858204a660ba736200db8aa04712
Renato Alushi <renato.alushi@raiffeisen.al>
2022-02-23 15:39:30 +0100

    add unit tests for LoanTesm class

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -13,5 +13,30 @@ namespace Loans.Tests
             var sut = new LoanTerm(1);
             Assert.That(sut.ToMonths(), Is.EqualTo(12));
         }
+
+        [Test]
+        public void SoreYears()
+        {
+            var sut = new LoanTerm(1);
+            Assert.That(sut.Years, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RespectValueEqualiy()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(1);
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+
+        [Test]
+        public void RespectValueInEqualiy()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(2);
+
+            Assert.That(a, Is.Not.EqualTo(b));
+        }
     }
 }
